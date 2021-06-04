c64299b82f69ee19ec09fe115c45b6475a1f9495
Dvoryadkin Pavel <dvoryadkinpavel@mail.ru>
2021-06-05 00:07:30 +0700

    first fixation

---
+++ src/test/java/ru/mail/dvoryadkinpavel/ExampleTest.java
@@ -0,0 +1,44 @@
+package ru.mail.dvoryadkinpavel;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class ExampleTest {
+    public static WebDriver driver;
+
+    /**
+     * осуществление первоначальной настройки
+     */
+    @BeforeClass
+    public static void setup() {
+        //определение пути до драйвера и его настройка
+        System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
+        //создание экземпляра драйвера
+        driver = new ChromeDriver();
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    /**
+     * тестовый метод для осуществления аутентификации
+     */
+    @Test
+    public void exampleTest() {
+        driver.get("http://google.ru");
+        Assert.assertTrue("Что-то пошло не так",driver.getCurrentUrl().contains("google"));
+    }
+    /**
+     * осуществление выхода из аккаунта с последующим закрытием окна браузера
+     */
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+        System.out.println("Test passed");
+    }
+}
